56a06aea8d949c74267e391dbbbb732b01c2eb83
tkell <thor.kell@mail.mcgill.ca>
2014-11-08 13:06:19 -0500

    Playing, stopping, looping

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var context = null
 var tempo = 120
 var sequence = []
+var playback = false
+var currentlyQueued = []
 
 function info(string) {
     console.log(string)
@@ -12,16 +14,43 @@ function playSound(when, buffer) {
   source.buffer = buffer
   source.connect(context.destination)
   source.start(when)
+  return source
+}
+
+function startSequencer() {
+    playback = true
+    sequencePlay(sequence, tempo)
+}
+
+function stopSequencer () {
+    playback = false
+    // will need to stop currently queued items...
+    for (var i = 0; i < currentlyQueued.length; i++) {
+        if (currentlyQueued[i] != null) {
+            currentlyQueued[i].stop()
+        }
+    }
+    currentlyQueued = []
 }
 
 function sequencePlay(sequence, tempo) {
     var sixteenthNote = 60.0 / tempo / 4.0
     var when = context.currentTime
     for (var i = 0; i < sequence.length; i++) {
+        if (playback == false) {
+            break
+        }
         if (sequence[i] != '') {
-            playSound(when, sequence[i])
+            queuedSound = playSound(when, sequence[i])
+            currentlyQueued.push(queuedSound)
         }
         when = when + sixteenthNote
+    }
+    if (playback == true) {
+        var totalTime = sixteenthNote * 16 * 1000 // seconds vs milleseconds
+        setTimeout(function() {
+            sequencePlay(sequence, tempo)
+        }, totalTime)
     }
 }
 
